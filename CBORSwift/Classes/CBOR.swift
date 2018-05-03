c5cdb9e1aa7916fe6747866da87440a377825355
Hassaniiii <>
2018-05-03 17:55:32 +0430

    Negative integer encoding is done

--- CBORSwift/Classes/CBOR.swift
+++ CBORSwift/Classes/CBOR.swift
@@ -8,13 +8,12 @@
 
 class CBOR: NSObject {
     
-    public class func encode(integer value: NSNumber) -> String {
+    public class func encode(integer value: NSNumber) -> [UInt8]? {
         let major = MajorTypes()
         major.set(type: .major0)
-        
         let header = major.get()
-        var rawBytes = [UInt8]()
         
+        var rawBytes = [UInt8]()
         if value.intValue >= 0 && value.intValue <= 23 {}
         else if value.intValue >= 24 && value.intValue <= 255 {
             rawBytes = 24.binary
@@ -28,12 +27,30 @@ class CBOR: NSObject {
         
         rawBytes.append(contentsOf: value.intValue.binary)
         let rawData = Data(bytes: rawBytes)
-        return Encoder.encode(value: rawData, header: header)
+        return Encoder.encode(value: rawData, header: header).data?.binary
     }
     
-    public class func encode(negative value: NSNumber) {
+    public class func encode(negative value: NSNumber) -> [UInt8]? {
+        let value = (value.intValue * -1) - 1
         let major = MajorTypes()
         major.set(type: .major1)
+        let header = major.get()
+        
+        var rawBytes = [UInt8]()
+        if value >= 0 && value <= 23 {}
+        else if value >= 24 && value <= 255 {
+            rawBytes = 24.binary
+        }
+        else if value >= 256 && value <= 65535 {
+            rawBytes = 25.binary
+        }
+        else if value >= 65536 && value <= 4294967295 {
+            rawBytes = 26.binary
+        }
+        
+        rawBytes.append(contentsOf: value.binary)
+        let rawData = Data(bytes: rawBytes)
+        return Encoder.encode(value: rawData, header: header).data?.binary
     }
     
     public class func encode(byteString value: Data) {
--- CBORSwiftTests/CBORSwiftTests.swift
+++ CBORSwiftTests/CBORSwiftTests.swift
@@ -36,97 +36,147 @@ class CBORSwiftTests: XCTestCase {
         major.set(type: .major5)
         XCTAssertEqual(Data(bytes: [0x01, 0x00, 0x01]), major.get())
     }
-    
+
+    //MARK:- Unsigned integer encoding
     func test_2_encodeSimpleInt() {
         var encoded = CBOR.encode(integer: NSNumber(value: 0))
         XCTAssertNotNil(encoded)
-        XCTAssertEqual("0", encoded)
-        XCTAssertEqual([0x00], encoded.data?.binary)
+        XCTAssertEqual([0x00], encoded)
         
         encoded = CBOR.encode(integer: NSNumber(value: 5))
         XCTAssertNotNil(encoded)
-        XCTAssertEqual("5", encoded)
-        XCTAssertEqual([0x05], encoded.data?.binary)
+        XCTAssertEqual([0x05], encoded)
         
         encoded = CBOR.encode(integer: NSNumber(value: 10))
         XCTAssertNotNil(encoded)
-        XCTAssertEqual("A", encoded)
-        XCTAssertEqual([0x0A], encoded.data?.binary)
+        XCTAssertEqual([0x0A], encoded)
         
         encoded = CBOR.encode(integer: NSNumber(value: 16))
         XCTAssertNotNil(encoded)
-        XCTAssertEqual("10", encoded)
-        XCTAssertEqual([0x10], encoded.data?.binary)
+        XCTAssertEqual([0x10], encoded)
 
         encoded = CBOR.encode(integer: NSNumber(value: 23))
         XCTAssertNotNil(encoded)
-        XCTAssertEqual("17", encoded)
-        XCTAssertEqual([0x17], encoded.data?.binary)
+        XCTAssertEqual([0x17], encoded)
     }
     
     func test_3_encodeStage1Int() {
         var encoded = CBOR.encode(integer: NSNumber(value: 24))
         XCTAssertNotNil(encoded)
-        XCTAssertEqual("1818", encoded)
-        XCTAssertEqual([0x18, 0x18], encoded.data?.binary)
-        
+        XCTAssertEqual([0x18, 0x18], encoded)
+
         encoded = CBOR.encode(integer: NSNumber(value: 25))
         XCTAssertNotNil(encoded)
-        XCTAssertEqual("1819", encoded)
-        XCTAssertEqual([0x18, 0x19], encoded.data?.binary)
-        
+        XCTAssertEqual([0x18, 0x19], encoded)
+
         encoded = CBOR.encode(integer: NSNumber(value: 125))
         XCTAssertNotNil(encoded)
-        XCTAssertEqual("187D", encoded)
-        XCTAssertEqual([0x18, 0x7D], encoded.data?.binary)
-        
+        XCTAssertEqual([0x18, 0x7D], encoded)
+
         encoded = CBOR.encode(integer: NSNumber(value: 178))
         XCTAssertNotNil(encoded)
-        XCTAssertEqual("18B2", encoded)
-        XCTAssertEqual([0x18, 0xB2], encoded.data?.binary)
-        
+        XCTAssertEqual([0x18, 0xB2], encoded)
+
         encoded = CBOR.encode(integer: NSNumber(value: 255))
         XCTAssertNotNil(encoded)
-        XCTAssertEqual("18FF", encoded)
-        XCTAssertEqual([0x18, 0xFF], encoded.data?.binary)
+        XCTAssertEqual([0x18, 0xFF], encoded)
     }
-    
+
     func test_4_encodeStage2Int() {
         var encoded = CBOR.encode(integer: NSNumber(value: 256))
         XCTAssertNotNil(encoded)
-        XCTAssertEqual("190100", encoded)
-        XCTAssertEqual([0x19, 0x01, 0x00], encoded.data?.binary)
+        XCTAssertEqual([0x19, 0x01, 0x00], encoded)
 
         encoded = CBOR.encode(integer: NSNumber(value: 300))
         XCTAssertNotNil(encoded)
-        XCTAssertEqual("19012C", encoded)
-        XCTAssertEqual([0x19, 0x01, 0x2C], encoded.data?.binary)
+        XCTAssertEqual([0x19, 0x01, 0x2C], encoded)
 
         encoded = CBOR.encode(integer: NSNumber(value: 1200))
         XCTAssertNotNil(encoded)
-        XCTAssertEqual("1904B0", encoded)
-        XCTAssertEqual([0x19, 0x04, 0xB0], encoded.data?.binary)
+        XCTAssertEqual([0x19, 0x04, 0xB0], encoded)
 
         encoded = CBOR.encode(integer: NSNumber(value: 3428))
         XCTAssertNotNil(encoded)
-        XCTAssertEqual("190D64", encoded)
-        XCTAssertEqual([0x19, 0x0D, 0x64], encoded.data?.binary)
+        XCTAssertEqual([0x19, 0x0D, 0x64], encoded)
 
         encoded = CBOR.encode(integer: NSNumber(value: 65500))
         XCTAssertNotNil(encoded)
-        XCTAssertEqual("19FFDC", encoded)
-        XCTAssertEqual([0x19, 0xFF, 0xDC], encoded.data?.binary)
+        XCTAssertEqual([0x19, 0xFF, 0xDC], encoded)
     }
 
     func test_5_encodeStage3Int() {
         var encoded = CBOR.encode(integer: NSNumber(value: 65536))
         XCTAssertNotNil(encoded)
-        XCTAssertEqual("1A00010000", encoded)
-        XCTAssertEqual([0x1A, 0x00, 0x01, 0x00, 0x00], encoded.data?.binary)
+        XCTAssertEqual([0x1A, 0x00, 0x01, 0x00, 0x00], encoded)
 
         encoded = CBOR.encode(integer: NSNumber(value: 42949295))
         XCTAssertNotNil(encoded)
-        XCTAssertEqual("1A028F5AAF", encoded)
-        XCTAssertEqual([0x1A, 0x02, 0x8F, 0x5A, 0xAF], encoded.data?.binary)
+        XCTAssertEqual([0x1A, 0x02, 0x8F, 0x5A, 0xAF], encoded)
     }
+
+    //MARK:- Negative integer encoding
+    func test_2_encodeSimpleNeg() {
+        var encoded = CBOR.encode(negative: NSNumber(value: -1))
+        XCTAssertNotNil(encoded)
+        XCTAssertEqual([0x20], encoded)
+        
+        encoded = CBOR.encode(negative: NSNumber(value: -7))
+        XCTAssertNotNil(encoded)
+        XCTAssertEqual([0x26], encoded)
+        
+        encoded = CBOR.encode(negative: NSNumber(value: -16))
+        XCTAssertNotNil(encoded)
+        XCTAssertEqual([0x2F], encoded)
+        
+        encoded = CBOR.encode(negative: NSNumber(value: -24))
+        XCTAssertNotNil(encoded)
+        XCTAssertEqual([0x37], encoded)
+    }
+    
+    func test_3_encodeStage1Neg() {
+        var encoded = CBOR.encode(negative: NSNumber(value: -25))
+        XCTAssertNotNil(encoded)
+        XCTAssertEqual([0x38, 0x18], encoded)
+        
+        encoded = CBOR.encode(negative: NSNumber(value: -96))
+        XCTAssertNotNil(encoded)
+        XCTAssertEqual([0x38, 0x5F], encoded)
+        
+        encoded = CBOR.encode(negative: NSNumber(value: -160))
+        XCTAssertNotNil(encoded)
+        XCTAssertEqual([0x38, 0x9F], encoded)
+        
+        encoded = CBOR.encode(negative: NSNumber(value: -256))
+        XCTAssertNotNil(encoded)
+        XCTAssertEqual([0x38, 0xFF], encoded)
+    }
+    
+    func test_4_encodeStage2Neg() {
+        var encoded = CBOR.encode(negative: NSNumber(value: -257))
+        XCTAssertNotNil(encoded)
+        XCTAssertEqual([0x39, 0x01, 0x00], encoded)
+        
+        encoded = CBOR.encode(negative: NSNumber(value: -382))
+        XCTAssertNotNil(encoded)
+        XCTAssertEqual([0x39, 0x01, 0x7D], encoded)
+        
+        encoded = CBOR.encode(negative: NSNumber(value: -3428))
+        XCTAssertNotNil(encoded)
+        XCTAssertEqual([0x39, 0x0D, 0x63], encoded)
+        
+        encoded = CBOR.encode(negative: NSNumber(value: -65500))
+        XCTAssertNotNil(encoded)
+        XCTAssertEqual([0x39, 0xFF, 0xDB], encoded)
+    }
+    
+    func test_5_encodeStage3Neg() {
+        var encoded = CBOR.encode(negative: NSNumber(value: -65537))
+        XCTAssertNotNil(encoded)
+        XCTAssertEqual([0x3A, 0x00, 0x01, 0x00, 0x00], encoded)
+        
+        encoded = CBOR.encode(negative: NSNumber(value: -42949295))
+        XCTAssertNotNil(encoded)
+        XCTAssertEqual([0x3A, 0x02, 0x8F, 0x5A, 0xAE], encoded)
+    }
+
 }
